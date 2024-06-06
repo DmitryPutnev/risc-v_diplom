aa25fa15328225b099079ddf92bea2be0a30f6e2
dmitry putnev <megadimoxa@yandex.ru>
2024-06-06 18:56:32 +0300

    Добавил тесты

---
+++ c/c-except/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Сборка программы
+make > /dev/null 2>&1
+
+# Запуск программы
+spike except > /dev/null 2>&1
+
+# Сохранение кода завершения
+exit_code=$?
+
+# Код ожидания
+expected_code=1
+
+# Цвета
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m"
+
+# Проверка кода завершения
+if [ $exit_code -eq $expected_code ]; then
+	echo -e "${GREEN}Test passed. Exit code: $exit_code${NC}"
+else
+	echo -e "${RED}Test failed/ Expected: $expected_code, Got: $exit_code${NC}"
+fi
+
+# Очистка файлов
+make clean > /dev/null 2>&1
